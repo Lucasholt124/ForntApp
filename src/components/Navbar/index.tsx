b89c630b9531d70ee91bc3cd149bd47cd8777c43
Lucas Aragão Tavares Mota <136651806+Lucasholt124@users.noreply.github.com>
2024-08-01 10:39:46 -0300

    Testando Brech

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -20,13 +20,13 @@ const Navbar = () => {
         <div className='collapse navbar-collapse' id='dscatalog-navbar'>
           <ul className='navbar-nav offset-md-2 main-menu'>
             <li>
-              <a href="link" className='active'>HOME</a>
+              <a href="link" className='active'>HOME.</a>
             </li>
             <li>
-              <a href="link">CATÁLOGO</a>
+              <a href="link">CATÁLOGO.</a>
             </li>
             <li>
-              <a href="link">ADMIN</a>
+              <a href="link">ADMIN.</a>
             </li>
           </ul>
         </div>
