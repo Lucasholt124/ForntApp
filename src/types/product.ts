854eda4df4c8ab9cd6ed166327c386c50e817efa
Lucas Aragão Tavares Mota <136651806+Lucasholt124@users.noreply.github.com>
2024-08-10 13:40:51 -0300

    Atualização
    Atualizando

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+import type { Category } from "./Category"
+
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imgUrl: string;
+  date: string;
+  categories: Category[];
+}
